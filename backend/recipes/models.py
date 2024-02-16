3c4287a5315e0d424138ef917c8b21509c540b0e
Дмитрий Рудаков <rud@kov19.ru>
2024-02-16 03:28:02 +0300

    Создал модели для тегов, ингредиентов, рецептов

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,101 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Tag(models.Model):
+    name = models.CharField(
+        'Название',
+        max_length=200,
+        unique=True
+    )
+    color = models.CharField(
+        'Цвет в HEX',
+        max_length=7,
+        unique=True
+    )
+    slug = models.CharField(
+        'Уникальный слаг',
+        max_length=200,
+        unique=True
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(
+        'Наименование ингредиента',
+        max_length=200,
+        unique=True
+    )
+    measurement_unit = models.CharField(
+        'Единицы измерения',
+        max_length=7,
+        unique=True
+    )
+
+    def __str__(self):
+        return f'{self.name} ({self.measurement_unit})'
+
+
+class Recipe(models.Model):
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        verbose_name='Список ингредиентов',
+        through='RecipeIngredient'
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        verbose_name='Список id тегов',
+        through='RecipeTag'
+    )
+    image = models.ImageField(
+        'Картинка, закодированная в Base64',
+    )
+    name = models.CharField(
+        'Название',
+        max_length=200,
+        unique=True
+    )
+    text = models.TextField(
+        'Описание',
+    )
+    cooking_time = models.PositiveSmallIntegerField(
+        'Время приготовления (в минутах)'
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE
+    )
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE
+    )
+    amount = models.PositiveSmallIntegerField(
+        'Количество в рецепте'
+    )
+
+    def __str__(self):
+        return f'{self.ingredient.name} в {self.recipe.name}'
+
+
+class RecipeTag(models.Model):
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE
+    )
+    tag = models.ForeignKey(
+        Tag,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f'{self.tag.name} в {self.recipe.name}'
